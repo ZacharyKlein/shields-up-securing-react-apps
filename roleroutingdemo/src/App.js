13fabe307d233044f238c693cabd7370cb75673d
Zachary Klein <zak@silver-chalice.com>
2017-11-25 23:45:10 -0600

    Added Authorized HOC

--- roleroutingdemo/src/App.js
+++ roleroutingdemo/src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom'
 import Home from "./Home";
 import Public from "./Public";
 import Restricted from "./Restricted";
 import Admin from "./Admin";
 import Login from "./Login";
+import Authorized from "./Authorized";
+import NavBar from "./NavBar";
 
 class App extends Component {
 
@@ -17,38 +18,38 @@ class App extends Component {
     super();
     this.state = {
       loggedIn: false,
-      role: ''
+      user: {
+        role: ''
+      }
     }
   }
 
-  loginUser = () => {this.setState({role: 'user', loggedIn: true})}
-  loginAdmin = () => {this.setState({role: 'admin', loggedIn: true})}
-  logout = () => {this.setState({role: '', loggedIn: false})}
+  loginUser = () => this.setState({user: {role: 'user'}, loggedIn: true});
+  loginAdmin = () => this.setState({user: {role: 'admin'}, loggedIn: true});
+  logout = () => this.setState({user: {role: ''}, loggedIn: false});
 
   render() {
+    const {user, loggedIn} = this.state;
+    const AdminRole = Authorized(['admin'], user);
+    const UserRole = Authorized(['user', 'admin'], user);
+
     return (
       <Router>
         <div className="App" style={{margin: '0 auto', width: '80%'}}>
           <header>
             <h1>Role-Based Routing</h1>
+            <p>Currently logged in: <strong>{loggedIn ? 'yes' : 'no'}</strong></p>
+            <p>Has Role: <strong>{user.role ? user.role : 'none'}</strong></p>
           </header>
-          <nav style={{float: 'left', textAlign: 'left'}}>
-            <h3>Navigation</h3>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/public">Public</Link></li>
-              <li><Link to="/restricted">Restricted</Link></li>
-              <li><Link to="/admin">Admin</Link></li>
-              <li><a href="#" onClick={this.logout}>Logout</a></li>
-            </ul>
-          </nav>
+
+          <NavBar logout={this.logout}/>
+
           <div style={{float: 'right', minWidth: '720px'}}>
             <Route path="/" exact component={Home}/>
-            <Route path="/login" component={Login} loginUser={this.loginUser} loginAdmin={this.loginAdmin}/>
+            <Route path="/login" render={() => <Login loginUser={this.loginUser} loginAdmin={this.loginAdmin} loggedIn={loggedIn}/>}/>
             <Route path="/public" component={Public}/>
-            <Route path="/restricted" component={Restricted}/>
-            <Route path="/admin" component={Admin}/>
+            <Route path="/restricted" component={UserRole(Restricted)}/>
+            <Route path="/admin" component={AdminRole(Admin)}/>
           </div>
         </div>
       </Router>
---
+++ roleroutingdemo/src/Authorized.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+const Authorized = (allowedRoles, user) => {
+   return (WrappedComponent) => {
+    return class WithAuthorization extends Component {
+
+      render() {
+        const {role} = user;
+        if (allowedRoles.includes(role)) {
+          return <WrappedComponent {...this.props} />
+        } else {
+          return <h1>No page for you!</h1>
+        }
+      }
+    }
+  }
+};
+
+
+export default Authorized;
