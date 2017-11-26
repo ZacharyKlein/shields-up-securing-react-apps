702cb1bf099bab394c802dfd05803ee266c05c7c
Zachary Klein <zak@silver-chalice.com>
2017-11-25 23:45:55 -0600

    Credits

--- roleroutingdemo/src/Home.js
+++ roleroutingdemo/src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
 const Home = () => {
-
   return <div>
-    <h1>Home page</h1>
+    <h1>Home Page</h1>
     <p>Anyone can view this content.</p>
+    <p>Authorized <a href="https://reactjs.org/docs/higher-order-components.html">Higher-Order Component</a> based on the approach described here: <br/>
+      <a href="https://hackernoon.com/role-based-authorization-in-react-c70bb7641db4">Role based authorization in React</a></p>
   </div>
 }
 
